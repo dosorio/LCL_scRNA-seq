7fc1aa6cfc13b6e56adff2fae95078e360dbd246
James Cai <jamescai@genomezoo.net>
2018-10-07 10:56:26 -0500

    .

---
+++ s11_expr_noise_scatter.m
@@ -0,0 +1,60 @@
+sortby="expr_level";
+i_common_code;
+
+
+X=GM12878_expr(:,cellcycleGM12878=="G1");
+Y=GM18502_expr(:,cellcycleGM18502=="G1");
+% Z=GM12878_expr(:,cellcycleGM12878=="S");
+
+%%
+
+
+figure;
+u=mean(X,2);
+cv=std(X,0,2)./u;
+
+% idx=~isnan(cv);
+% gl123x=gl123(idx);
+% gl123descx=gl123desc(idx);
+% u=u(idx);
+% cv=cv(idx);
+
+loglog(u,cv,'o');
+hold on
+i=1e-4:1e2;
+a=0.5;
+j=10.^-(log10(i)*a);
+plot(i,j,'-rs');
+%j2=i.^-0.5;
+%loglog(i,j2,'-o');
+grid on
+xlabel('Mean')
+ylabel('CV')
+
+% loglog(mean(Y,2),std(Y,[],2)./mean(Y,2),'.')
+% v=log10(cv)-(-0.5*log10(u));
+
+%{
+For single-cell gene expression data, in the
+ideal condition all genes should obey CV = u^1/2 [11],
+following a Poisson distribution as depicted by a black
+diagonal line in log(?) vs log(CV) plot shown in Fig. 2.
+BMC Genomics 2016, 17(Suppl 7):508
+
+%}
+
+%%
+%{
+v1=log10(std(X,0,2)./mean(X,2))-(-0.5*log10(mean(X,2)));
+v2=log10(std(Y,0,2)./mean(Y,2))-(-0.5*log10(mean(Y,2)));
+
+figure;
+scatter(v1,v2,'.')
+refline(1)
+box on
+xlabel('EUR residual cv')
+ylabel('AFR residual cv')
+dt = datacursormode;
+dt.UpdateFcn = {@i_myupdatefcn,gl123};
+%}
+
--- s9_visualize_matrix.m
+++ s9_visualize_matrix.m
@@ -1,18 +1,60 @@
 sortby="expr_level";
 i_common_code;
 
+ethid="EUR";
+switch ethid
+    case "EUR"
+X=GM12878_expr; x=cellcycleGM12878; x1=i1; x2=i2; x3=i3;
+    case "AFR"
+X=GM18502_expr; x=cellcycleGM18502; x1=j1; x2=j2; x3=j3;
+end
+
 %%
-% close all
-% A=GM12878_expr;
-A=GM12878_expr; a=cellcycleGM12878;
-% A=GM18502_expr; a=cellcycleGM18502;
-[x,i]=sort(a);
+% figure;
+% hold on
+% for k=0.9:-0.05:0.5
+%     plot(k, sum((sum(A>0,2)/size(A,2))>k),'ko');
+% end
+
+%%
+%{
+[xs,i]=sort(x);
 figure;
-imagesc(log10(1+A(1:8000,i)))
-vline(find(x=="G2M", 1 ),'y-')
-vline(find(x=="S", 1 ),'g-')
-title('G1 |  G2/M | S')
+imagesc(log10(1+X(1:2000,i)))
+vline(find(xs=="G2M", 1 ),'y-')
+vline(find(xs=="S", 1 ),'g-')
+title('G1  |  G2/M  |   S')
 colorbar;
 xlabel('Cell')
 ylabel('Gene')
+%}
 
+%%
+figure;
+A1=X(:,x1); [~,i]=sort(sum(A1,1),'descend'); A1=A1(:,i);
+A2=X(:,x2); [~,i]=sort(sum(A2,1),'descend'); A2=A2(:,i);
+A3=X(:,x3); [~,i]=sort(sum(A3,1),'descend'); A3=A3(:,i);
+
+XA=[A1 A3 A2];
+imagesc(log10(1+XA(201:300,:)))
+% vline(find(x=="G2M", 1 ),'y-')
+% vline(find(x=="S", 1 ),'y-')
+vline(size(A1,2),'y-');
+vline(size(A1,2)+size(A3,2),'y-');
+title('G1  |  S  |   G2/M')
+colorbar;
+xlabel('Cell')
+ylabel('Gene')
+dt = datacursormode;
+dt.UpdateFcn = {@myupdatefcn,gl123,gl123desc,200};
+
+
+function txt = myupdatefcn(~,event_obj,g,gd,id)
+% Customizes text of data tips
+pos = event_obj.Position;
+% idx = event_obj.DataIndex;
+% i_plotsiglegene(idx,g);
+num2str(pos(2)+id)
+txt = {[char(g(pos(2)+id)) ' - ' char(gd(pos(2)+id))]};
+% txt={num2str(pos(2))}
+end
