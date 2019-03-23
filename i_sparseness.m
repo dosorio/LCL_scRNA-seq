026e4918e4d8e476c0c11d412f6c96132b5e96f6
Cai <jcai@cvm.tamu.edu>
2019-03-23 00:06:38 -0500

    .

--- i_sparseness.m
+++
@@ -1,6 +0,0 @@
-function s=i_sparseness(x)
-n=numel(x);
-s=(sqrt(n)-sum(abs(x))/norm(x))/(sqrt(n)-1);
-
-% http://www.jmlr.org/papers/volume5/hoyer04a/hoyer04a.pdf 
-% https://github.com/aludnam/MATLAB/tree/master/nmfpack
